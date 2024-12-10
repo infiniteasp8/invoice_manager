caff0303df9c9b29704e3b56883dc0ad2c34547e
Pragyanand022 <99154355+Pragyanand022@users.noreply.github.com>
2024-12-10 19:41:30 +0530

    making serverless exports ready

--- backend/index.js
+++ backend/index.js
@@ -200,6 +200,8 @@ app.post('/store-products', async (req, res) => {
   
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
+
+module.exports = app;
