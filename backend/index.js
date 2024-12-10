45d1903a718184edd747ce80ff5e67bff6e4e2c9
Pragyanand022 <99154355+Pragyanand022@users.noreply.github.com>
2024-12-10 19:31:22 +0530

    ready for vercel hosting

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ const pool = new Pool({
   user: 'postgres',      // Replace with your PostgreSQL username
   host: 'localhost',          // Replace with your PostgreSQL host
   database: 'invoicer',  // Replace with your PostgreSQL database name
-  password: 'pnsql',  // Replace with your PostgreSQL password
+  password: DB_Pass,  // Replace with your PostgreSQL password
   port: 5432,                 // Default PostgreSQL port
 });
 
